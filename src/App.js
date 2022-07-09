0417fd6ddcd19d618ffba281c335dd63f6bd485b
OkoyeCharles <okoyecharles509@gmail.com>
2022-07-09 07:56:50 +0100

    Install React-router

--- src/App.js
+++ src/App.js
@@ -2,14 +2,18 @@ import React from 'react';
 import Header from './Header';
 import Home from './Home';
 import './App.css';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 
 function App() {
   return (
     // BEM
-    <div className="App">
-      <Header/>
-      <Home/>
-    </div>
+    <Router>
+      <div className="App">
+        <Routes>
+          <Route exact path="/" element = {<div><Header/><Home/></div>} />
+        </Routes>     
+      </div>
+    </Router>
   );
 }
 
