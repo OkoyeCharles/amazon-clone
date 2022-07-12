5133b41e1cce20ecca3e01a1b6b14121bc975a87
OkoyeCharles <okoyecharles509@gmail.com>
2022-07-12 13:50:32 +0100

    Style Payment page

--- src/Payment.js
+++ src/Payment.js
@@ -1,9 +1,57 @@
 import React from 'react'
+import { Link } from 'react-router-dom';
+import CheckoutProduct from './CheckoutProduct';
 import './Payment.css';
+import { useStateValue } from './StateProvider';
 
 function Payment() {
+  const [{basket, user}, dispatch] = useStateValue();
+
   return (
-    <div>Payment</div>
+    <div className="payment">
+      <div className="payment__container">
+        <h1>
+          Checkout (<Link to="/checkout">{basket.length || 0} items</Link>)
+        </h1>
+
+        <div className="payment__section">
+          <div className="payment__title">
+            <h3>Delivery Address</h3>
+          </div>
+          <div className="payment__address">
+            <p>{user ? user.email : 'User'}</p>
+            <p>123 React Lane</p>
+            <p>Los Angeles, CA</p>
+          </div>
+        </div>
+
+        <div className="payment__section">
+          <div className="payment__title">
+            <h3>Review Items and Delivery</h3>
+          </div>
+          <div className="payment__items">
+            {basket.map(item => 
+              <CheckoutProduct 
+                id = {item.id}
+                image = {item.image}
+                title = {item.title}
+                rating = {item.rating}
+                price = {item.price}
+              />
+            )}
+          </div>
+        </div>
+
+        <div className="payment__section">
+          <div className="payment__title">
+            <h3>Review Items and Delivery</h3>
+          </div>
+          <div className="payment__details">
+              {/* Stripe */}
+          </div>
+        </div>
+      </div>
+    </div>
   )
 }
 
